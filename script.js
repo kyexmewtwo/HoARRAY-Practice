366911ed27ead0f11401cb73e8afc095fbcc4199
kyexmewtwo <45476037+kyexmewtwo@users.noreply.github.com>
2019-04-29 09:58:34 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,43 @@
+/*
+	PART 1 
+ */
+
+// Task 1: Update the variable songList so that it is an array with at least 3 song titles.
+var songList = ["The Whims of Fate", "Beneath the Mask", "Rolling Start"];
+console.log(songList);
+
+// Task 2: Update the variable classList so that it is an array with 4 ScriptEd students (including yourself) in your class.
+var classList = ["Kayla", "Prass", "Donell", "Nassair"];
+console.log(classList);
+
+// Task 3: Update the variable teacherList so that it is an array with all the ScriptEd adults in your class.
+var teacherList = ["n/a", "n/a", "n/a"];
+console.log(teacherList);
+
+// Task 4: Update the variable familyAges so that it is an array with a list of ages of your family members.
+var familyAges = ["1","5","3","9"];
+console.log(familyAges);
+
+
+
+/*
+	PART 2 
+ */
+
+// Task 5: Update the variable favSong to retrieve the array element of your favorite song in the songList array (Task 1). 
+var favSong = songList[1];
+console.log(favSong);
+
+// Task 6: Update the variable bestStudent to retrieve your name from the classList array (Task 2).
+var bestStudent = classList[1];
+console.log(bestStudent);
+
+// Task 7: Update the variable tallestTeacher to retrieve the array element of the tallest teacher in the teacherList array (Task 3).
+var tallestTeacher = teacherList[1];
+console.log(tallestTeacher);
+
+// Task 8: Update the variable oldestMemeber to retrieve the array element of the oldest member of your family in the familyAges array. (Task 4)
+var oldestMember = familyAges[3];
+console.log(oldestMember);
+
+
